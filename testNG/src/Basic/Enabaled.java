45a920d23583951b9461990e2e6b9457d8bc0419
Lenovo <Lenovo@DESKTOP-FG1L4L8>
2022-12-08 00:37:59 +0530

    push

---
+++ testNG/src/Basic/Enabaled.java
@@ -0,0 +1,23 @@
+package Basic;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Enabaled 
+{
+	@Test(invocationCount = 2, priority = 2)
+	public void test1()
+	{
+		Reporter.log("TC IS PASS__1",true);	
+	}
+	@Test(priority = 4, enabled = false)//enabled
+	public void test2()
+	{
+		Reporter.log("TC IS FAIL__2",true);	
+	}
+	@Test(priority = 1)
+	public void test3()
+	{
+		Reporter.log("TC IS FAIL__3",true);	
+	}
+}
